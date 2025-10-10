7fefce7ceecd5385f3a187c41534a73dda03c6a8
Frade11 <gurdisartemii@gmail.com>
2025-10-10 21:33:21 +0300

    version 1.6 - added link to github profile with js

--- srcipt.js
+++ srcipt.js
@@ -1,5 +1,26 @@
-const menuIcon = document.querySelector('.mobile-nav img')
+const menuIcon = document.querySelector('.mobile-nav img');
 const menuState = document.querySelector('.nav-list');
 menuIcon.addEventListener('click', function(){
     menuState.classList.toggle('active');
 });
+
+const contactLink = document.querySelectorAll('.contacts .contact img');
+
+contactLink.forEach(icon =>{
+    icon.addEventListener('click', () => {
+        switch(icon.className){
+            case 'telegram':
+                window.open(' ','_blank');
+                break;
+            case 'github':
+                window.open('https://github.com/Frade11', '_blank');
+                break;
+            case 'instagram':
+                window.open('','_blank');
+                break;
+            case 'whatsapp':
+                window.open(' ','_blank');
+                break;
+        }
+    })
+})
