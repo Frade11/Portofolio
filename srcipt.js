f598f748535f13bbfd24601828b9ea52a76cc62b
Frade11 <gurdisartemii@gmail.com>
2025-10-15 18:52:44 +0300

    version 1.9 - Skill page with scroll animation

--- srcipt.js
+++ srcipt.js
@@ -22,3 +22,18 @@ contactLink.forEach(contact =>{
         }
     })
 })
+
+let cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('active');
+        }
+    });
+},
+{
+    threshold: 0.5
+});
+
+    cards.forEach(card => observer.observe(card));
